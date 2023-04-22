b4139bd96553765ff09c756fb5b810984a5b7250
Nguyễn Tuấn Lộc <74365102+NgTuanLoc@users.noreply.github.com>
2023-04-22 10:44:37 +0700

    first setup

---
+++ CreationalPatterns/Singleton.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreationalPatterns
+{
+  public class Singleton
+  {
+    public static void Log()
+    {
+      Console.WriteLine("HELOOO");
+    }
+  }
+}
---
+++ Main/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using CreationalPatterns;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.Clear();
+    Singleton.Log();
+    Console.WriteLine("HI");
+  }
+}
